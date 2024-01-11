a64f83a0e00d80e037da5af683e849b548d3a761
Stefan Pla <spla@icloud.com>
2024-01-11 15:24:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", 'victory');
+newHeader.innerHTML="STEFAN is the champion";
